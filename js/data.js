fa5fd53491e4f8cf711a82587d9201f820c807e6
Noah Varghese <varghese.noah@gmail.com>
2020-04-10 21:26:19 -0400

    populate table

--- js/data.js
+++ js/data.js
@@ -31,6 +31,14 @@ function populateProjectTable(data)
 {
     for ( i = 0; i < data.length; i++ )
     {
-        $('#projectTable tr:last').after('<tr><td>' + data[i]);
+        $('#projectTable tr:last').after('<tr><td>' + data[i].project_number + '</td><td>' + 
+                                            data[i].project_name + '</td><td>' + data[i].project_description + '</td><td>' + 
+                                            data[i].project_type + '</td><td>' + data[i].start_date + '</td><td>' + 
+                                            data[i].end_date + '</td><td>' + data[i].est_duration + '</td><td>' + 
+                                            data[i].est_project_cost + '</td><td>' + data[i].act_project_cost + '</td><td>' + 
+                                            data[i].est_effort + '</td><td>' + data[i].act_effort + '</td><td>' + dat[i].est_loc + '</td><td>' + 
+                                            data[i].act_loc + '</td><td>' + data[i].est_fp + '</td><td>' + data[i].act_fp + '</td><td>' + 
+                                            data[i].avg_cost_per_person_hour + '</td><td>' + data[i].avg_staffing_level + '</td><td>' + 
+                                            data[i].design_review_hours + '</td><td>' + data[i].dev_lang + '</td>');
     }
 }
