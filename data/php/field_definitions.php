cd51a51e049ad377063f91c1010a8e5fc42eed4f
Noah Varghese <varghese.noah@gmail.com>
2020-04-11 15:50:28 -0400

    merged changes from PorsheBranch

---
+++ data/php/field_definitions.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once "../config/db.php";
+
+if ( $_SERVER['REQUEST_METHOD'] === 'GET' )
+{  
+    if ( ! $_GET['column'] )
+    {
+        try
+        {
+            $command = "SELECT * FROM field_definitions";
+            $stmt = $dbh->prepare($command);
+            $stmt->execute();  
+        }
+        catch (Exception $e)
+        {
+            die("Error: Could not connect: " . $e->getMessage());
+        }
+        $result = $stmt->fetchAll();
+        echo json_encode($result);
+    }
+}
